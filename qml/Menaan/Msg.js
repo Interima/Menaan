edcf3b5692747568bf76631f13604e6a1c8309bd
interima <interima.x2@gmail.com>
2013-01-27 00:36:27 +0400

    Porting to Qt5 and CMake 2.8.9

--- qml/Menaan/Msg.js
+++
@@ -1,8 +0,0 @@
-//error message
-function errorMsg(str)
-{
-    var comp = Qt.createComponent("Message.qml");
-    var obj = comp.createObject(main);
-    obj.titleText = qsTr("Mena'an");
-    obj.dataText = str+qsTr("\n\nClick to close.");
-}
